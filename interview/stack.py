d96823ca25089cec3ec80de054cd9063cde3652f
Alexander Godov <godovav@oiate.ru>
2023-09-07 23:34:16 +0300

    Edit solution

--- interview/stack.py
+++ interview/stack.py
@@ -8,8 +8,7 @@ class Stack(deque):
         else:
             return False
 
-    # Здесь ожидаем строку в контексте второго задания
-    def push(self, element: str) -> None:
+    def push(self, element) -> None:
         self.append(element)
 
     # Данный метод доступен при наследовании
@@ -21,3 +20,48 @@ class Stack(deque):
     
     def size(self):
         return self.__len__()
+    
+
+def check_balance(string: str) -> None:
+    stack = Stack()
+
+    for sub_str in string.strip(''):
+        stack.append(sub_str)
+
+    l_stack = Stack()
+    r_stack = Stack()
+    handler = None
+
+    for _ in range(stack.size()):
+        item = stack.pop()
+
+        if item in ('[', '{', '('):
+            l_stack.push(item)
+        else:
+            r_stack.push(item)
+
+        if l_stack.size() == r_stack.size():
+            r_stack.reverse()
+
+    row_delimiter = 50 * "-"
+    print(row_delimiter)
+
+    
+if __name__ == '__main__':
+    balanced_args = [
+        "(((([{}]))))",
+        "[([])((([[[]]])))]{()}",
+        "{{[()]}}"
+    ]
+
+    disbalanced_args = [
+        "}{}",
+        "{{[(])]}}",
+        "[[{())}]"
+    ]
+    n = 1
+    for balanced_item, disbalanced_item in zip(balanced_args, disbalanced_args):
+        print(n)
+        n += 1
+        check_balance(balanced_item)
+        check_balance(disbalanced_item)
